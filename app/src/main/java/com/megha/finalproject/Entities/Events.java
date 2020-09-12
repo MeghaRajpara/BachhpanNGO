062a7cdd4f6cfeafddf14534b57843a53bebbaf5
Meghaben Kantilal Rajpara <mrajpara1@isimtl.local>
2020-09-11 23:05:15 -0400

    Added event entitie and Bachhpan interface

---
+++ app/src/main/java/com/megha/finalproject/Entities/Events.java
@@ -0,0 +1,62 @@
+package com.megha.finalproject.Entities;
+
+import java.util.Date;
+
+public class Events {
+
+    private String event_name;
+    private int id;
+    private Date date;
+    private String address;
+    private String descriptions;
+    private int likes;
+
+
+    public String getEvent_name() {
+        return event_name;
+    }
+
+    public void setEvent_name(String event_name) {
+        this.event_name = event_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+}
---
+++ app/src/main/java/com/megha/finalproject/Service/Bachhpan.java
@@ -0,0 +1,4 @@
+package com.megha.finalproject.Service;
+
+public interface Bachhpan {
+}
